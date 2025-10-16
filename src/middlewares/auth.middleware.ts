899d9944ce313369b299204c8163de882c26d4fa
Chris-mucyo <chrisxeventy6@gmail.com>
2025-10-16 20:05:24 +0200

    another

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -33,5 +33,5 @@ export const adminOnly = (req: AuthRequest, res: Response, next: NextFunction) =
   if (req.user?.role !== "admin") {
     return res.status(403).json({ message: "Admin access only" });
   }
-  next();
+  return next();
 };
